16ee6a6b3cdfaaacf0f345528778c78743c0bffe
John Dahl <john.dahl@gmail.com>
2022-01-25 12:17:51 +0100

    base sizes

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,29 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  large: {
+    '--borderRadius': '8px',
+    '--padding': '4px'
+  }
+}
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = SIZES[size];
+  return <Wrapper style={styles}>{value}</Wrapper>;
 };
+
+const Wrapper = styled.div`
+  --borderRadius: 4px;
+  --padding: 0;
+
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: var(--borderRadius);
+  padding: var(--padding);
+  &::before {
+    content: '';
+    background-color: ${COLORS.primary};
+  }
+`;
 
 export default ProgressBar;
