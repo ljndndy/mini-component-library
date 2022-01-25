a45689ee94b7a0f415801d319cfd469f19873ab8
John Dahl <john.dahl@gmail.com>
2022-01-25 12:44:47 +0100

    working bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -8,25 +8,46 @@ import VisuallyHidden from '../VisuallyHidden';
 const SIZES = {
   large: {
     '--borderRadius': '8px',
-    '--padding': '4px'
+    '--padding': '4px',
+    '--height': '24px'
+  },
+  medium: {
+    '--height': '12px'
+  },
+  small: {
+    '--height': '8px'
   }
 }
 
 const ProgressBar = ({ value, size }) => {
   const styles = SIZES[size];
-  return <Wrapper style={styles}>{value}</Wrapper>;
+
+  let progress = value*1;
+  if (progress < 0) {progress = 0;}
+  if (progress > 100) {progress = 100;}
+
+  return <Wrapper style={styles} progress={progress}>{value}</Wrapper>;
 };
 
 const Wrapper = styled.div`
-  --borderRadius: 4px;
+  --defaultBorderRadius: 4px;
+  --borderRadius: --defaultBorderRadius;
   --padding: 0;
 
-  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
   border-radius: var(--borderRadius);
   padding: var(--padding);
+
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+
+  height: var(--height);
+
   &::before {
     content: '';
+    display: block;
+    height: 100%;
+    width: ${props => props.progress + '%'};
     background-color: ${COLORS.primary};
+    border-radius: var(--defaultBorderRadius);
   }
 `;
 
