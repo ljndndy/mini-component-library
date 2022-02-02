042ced9d71b140be9c22da877ab507a9fca27b7f
John Dahl <john.dahl@gmail.com>
2022-02-02 13:21:33 +0100

    IconInput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,19 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    stroke: 1,
+    fontSize: 14,
+    height: 16
+  },
+  large: {
+    stroke: 2,
+    fontSize: 18,
+    height: 21
+  }
+};
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +26,64 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
-};
+  
+  const styles = SIZES[size];
+  
+  return (
+  <Wrapper style={{ '--width': width + 'px'}}>
+    <VisuallyHidden>{label}</VisuallyHidden>
+    <IconWrapper style={{'--size': styles.height + 'px' }}>
+      <Icon id={icon} size={styles.height} strokeWidth={styles.stroke}></Icon>
+    </IconWrapper>
+    <TextInput style={{'--height': styles.height + 'px', '--fontSize': styles.fontSize / 16 + 'rem', '--borderSize': styles.stroke + 'px'}} type="text" placeholder={placeholder}></TextInput>
+  </Wrapper>);
+};  
+
+const Wrapper = styled.div`
+  position: relative;
+  display: inline-block;
+  width: var(--width);
+
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  width: var(--size);
+  height: var(--size);
+
+  position: absolute;
+  left: 0;
+  top: 0;
+  margin: auto;
+
+  pointer-events: none;
+`;
+
+const TextInput = styled.input`
+  border: none;
+  border-bottom: var(--borderSize) solid ${COLORS.black};
+
+  padding: 0;
+  padding-left: 24px;
+  padding-bottom: 4px;
+
+  width: 100%;
+
+  font-size: var(--fontSize);
+  line-height: var(--height);
+  color: inherit;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+  }
+
+  &:focus {
+    outline-offset: 2px;
+  }
+`;
 
 export default IconInput;
